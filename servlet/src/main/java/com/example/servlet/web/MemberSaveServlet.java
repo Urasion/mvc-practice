af35ecd90dbd19ae833ed940f46768fb2bbd6d33
JEONGJIWON\wldnj <wldnjs9705@gmail.com>
2023-04-02 16:01:14 +0900

    no message

---
+++ servlet/src/main/java/com/example/servlet/web/MemberSaveServlet.java
@@ -0,0 +1,26 @@
+package com.example.servlet.web;
+
+import com.example.servlet.domain.member.Member;
+import com.example.servlet.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+
+    }
+}
